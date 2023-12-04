ecc79bfb722624098fd9a84bfe6419ec969326b6
Joselin <joselinargelm@gmail.com>
2023-12-03 23:53:36 -0300

    funciones

---
+++ Funciones/1_funcion.js
@@ -0,0 +1,9 @@
+function example(a, b, c){
+    return a+b+c
+    }
+
+    //Expresion de funcion 
+
+example = function (a,b,c){
+    return a+b+c
+}
---
+++ Funciones/2_arrow.js
@@ -0,0 +1,6 @@
+let suma = function(a, b){
+    return a + b
+    }
+
+    //Arrow funcion 
+    suma = (a,b) => (a,b)
